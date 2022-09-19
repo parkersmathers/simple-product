c9f80954bd398802ba40306c728813a7d05baad9
parkersmathers <parker.smathers@gmail.com>
2022-09-19 18:15:10 -0400

    deploy to netlify

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./app/store";
 
-if (process.env.NODE_ENV === "development") {
-  const { worker } = require("./mocks/browser");
-  worker.start();
-}
+const { worker } = require("./mocks/browser");
+worker.start();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
