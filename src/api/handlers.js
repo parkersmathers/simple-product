c8ecbbf36ca1551d420d39fcdb1e4a62b6cc0c27
parkersmathers <parker.smathers@gmail.com>
2022-09-20 13:20:42 -0400

    remove console.logs

--- src/api/handlers.js
+++ src/api/handlers.js
@@ -4,10 +4,8 @@ import db from "./db.json";
 export const handlers = [
   // Handles GET products/:id request
   rest.get(`/products/:id`, (req, res, ctx) => {
-    console.log("req.params :>> ", req.params);
     // mock findbyid in db
     const data = db.find((d) => d.id === req.params.id);
-    console.log("data :>> ", data);
     return res(ctx.status(200), ctx.json(data));
   }),
 ];
