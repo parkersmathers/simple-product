c1965f81f3c1a0b2bf43d296fb5c9342ae4f365b
parkersmathers <parker.smathers@gmail.com>
2022-10-05 14:52:23 -0400

    tweak header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,9 @@
-import React from "react";
-import logo from "../logo.svg";
+import React from 'react';
 
 const Header = () => {
   return (
-    <header className="navbar navbar-fixed navbar-bordered p-4">
-      <img className="navbar-brand-logo" src={logo} alt="logo" />
+    <header className="navbar navbar-fixed navbar-bordered py-3 px-4">
+      <h2 className="navbar-brand-logo mb-0 text-white">Simple</h2>
     </header>
   );
 };
